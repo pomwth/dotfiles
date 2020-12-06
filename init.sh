82f8c239e44302360f052a5c0ac9279f3dcbb05c
Wallop Th <pom.wallop.th@gmail.com>
2020-12-06 23:07:33 +0700

    Update SH variable.

--- init.sh
+++ init.sh
@@ -3,17 +3,23 @@
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 UNI=$(date +'%Y%m%d%H%M%S')
+HOME=~
+DOTFILES=.files
 
 # Link dot file folder
-rm -Rf ~/.files
-ln -fs $DIR/configs ~/.files
+rm -Rf $HOME/$DOTFILES
+ln -fs $DIR/configs $HOME/$DOTFILES
 
 # zsh
-rm -f ~/.zshrc
-ln -fs ~/.files/zsh/.zshrc ~/.zshrc
+rm -f $HOME/.zshrc
+ln -fs $HOME/$DOTFILES/zsh/.zshrc $HOME/.zshrc
 
 sudo apt install tmux -y
-rm -f ~/.tmux
-rm -f ~/.tmux.conf
-ln -fs ~/.files/tmux ~/.tmux
-ln -fs ~/.tmux/tmux.conf ~/.tmux.conf
+rm -f $HOME/.tmux
+rm -f $HOME/.tmux.conf
+ln -fs $HOME/$DOTFILES/tmux $HOME/.tmux
+ln -fs $HOME/.tmux/tmux.conf $HOME/.tmux.conf
+
+rm -Rf $HOME/.config/nvim
+mkdir -p $HOME/.config
+ln -fs $HOME/$DOTFILES
