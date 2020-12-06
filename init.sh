aa4aba5f9352ddb246a9c6820e19c0d2657ed072
Wallop Th <pom.wallop.th@gmail.com>
2020-12-06 19:30:01 +0700

    feat(install): Install tmux plugins

--- init.sh
+++ init.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 UNI=$(date +'%Y%m%d%H%M%S')
 
 # Link dot file folder
-mv ~/.files ~/.bck/.files-bck$UNI
+rm -Rf ~/.files
 ln -fs $DIR/configs ~/.files
 
 # zsh
-mv ~/.zshrc ~/.bck/.zshrc-bck$UNI
+rm -f ~/.zshrc
 ln -fs ~/.files/zsh/.zshrc ~/.zshrc
 
 sudo apt install tmux -y
-mv ~/.tmux.conf ~/.bck/.tmux.conf-bck$UNI
-ln -fs ~/.files/tmux/tmux.conf ~/.tmux.conf
-
+rm -f ~/.tmux
+rm -f ~/.tmux.conf
+ln -fs ~/.files/tmux ~/.tmux
+ln -fs ~/.tmux/tmux.conf ~/.tmux.conf
